f5d8c46b2742125830193d1b74a7ba8771dc00b6
Jonathan M. Lees <jonathan.lees@unc.edu>
2008-01-17 00:00:00 +0000

    version 1.0

---
+++ R/FANCY.TOMO.R
@@ -0,0 +1,86 @@
+`FANCY.TOMO` <-
+function(MOD, i, COL=NULL, LIM=NULL, MAP=NULL, MAPLIM=NULL,
+                     STA=NULL, staparams=list(col='green', pch=6, cex=.8, name=FALSE),
+                     PTS=NULL, ptsparams=list(col='green', pch=6, cex=.8, name=FALSE),
+                     TIT="Layer", UNITS="", bkgr="DarkSlateGray4" )
+  {
+
+    require(GEOmap)
+
+    if(missing(COL)) { COL=tomo.colors(100) }
+    if(missing(LIM)) { LIM=NULL }
+    if(missing(MAP)) { MAP=NULL }
+    if(missing(MAPLIM)) { MAPLIM=NULL }
+    
+    if(missing(STA)) { STA=NULL }
+    if(missing(staparams)) { staparams=list(col='green', pch=6, cex=.8, name=FALSE) }
+
+     if(missing(PTS)) { PTS=NULL }
+    if(missing(ptsparams)) { ptsparams=list(col='green', pch=6, cex=.8, name=FALSE) }
+
+   
+    if(missing(UNITS)) { UNITS="%" }
+      if(missing(bkgr)) { bkgr="DarkSlateGray4" }
+    
+    if(missing(TIT)) { TIT= paste(sep=' ', "LAYER=", i, "Depth", MOD$D[i], "-", MOD$D[i+1]) }
+
+    
+    
+    pltomo(MOD$x,MOD$y,MOD$MOD,i, COL, zlim=LIM, bkgr=bkgr, xlab="km", ylab="km")
+
+    if(is.null(LIM)) { LIM= range(MOD$MOD[[i]], na.rm=TRUE) } 
+    
+    HOZscale(LIM , col=COL  , units=UNITS, SIDE=1)
+    
+### HOZscale(MOD$MOD[[i]] , col=tomocolors  , units="%", SIDE=2)
+
+    
+###  image.SCALE(MOD$MOD[[i]] , col=rainbow(100), nlab=2)	
+    ## PROJmap(JAPmap,  ADD=TRUE, COL=TRUE)
+    if(!is.null(MAP))
+      {
+        ##  plotGEOmapXY(MAP, LIM=MAPLIM   , PROJ =MAP$PROJ ,   add=TRUE)
+         plotGEOmapXY(MAP, PROJ=MAP$PROJ, add=TRUE, xpd=FALSE )
+      }
+    if(!is.null(STA))
+      {
+        if(is.null(STA$FLAG)) { sflag = rep(TRUE, length(STA$x))} else { sflag = STA$FLAG }
+        
+        pointsGEOmapXY(STA$lat[sflag], STA$lon[sflag], PROJ=MAP$PROJ, pch=staparams$pch, col=staparams$col, cex=staparams$cex)
+
+        if(identical(staparams$name, TRUE))
+          {
+            
+            textGEOmapXY(STA$lat[sflag], STA$lon[sflag], labels=STA$name[sflag], PROJ=MAP$PROJ, pos=3, cex=staparams$cex)
+          }
+        
+
+        
+      }
+
+    if(!is.null(PTS))
+      {
+        if(is.null(PTS$FLAG)) { sflag = rep(TRUE, length(PTS$lat))} else { sflag = PTS$FLAG }
+        pointsGEOmapXY(PTS$lat[sflag], PTS$lon[sflag], PROJ=MAP$PROJ, pch=ptsparams$pch, col=ptsparams$col, cex=ptsparams$cex)
+
+        if(identical(ptsparams$name, TRUE))
+          {
+
+            textGEOmapXY(PTS$lat[sflag], PTS$lon[sflag], PROJ=MAP$PROJ, PTS$name[sflag], pos=3, cex=ptsparams$cex)
+          }
+        
+      }
+
+
+       
+    ##	text(STA$x[STAFLAG], STA$y[STAFLAG], labels=STA$nam[STAFLAG], pos=3, col='green', cex=.65)
+   ## print(TIT)
+    
+  	 if(!is.null(TIT))
+    	  {
+         title(main=TIT, sub=TIT)
+   	   }
+    
+
+  }
+
---
+++ R/SHOWTOMO.R
@@ -0,0 +1,57 @@
+`SHOWTOMO` <-
+function(MOD, colmap=topo.colors(100),  zlim=NULL,  MAP=NULL, I=1, J=2, bkgr="white"  )
+{
+  if(missing(colmap)) { colmap=tomo.colors(100) }
+  if(missing(MAP)) { MAP=NULL }
+  if(missing(I)) { I=1 }
+  if(missing(J)) { J=length(MOD$MOD) }
+  if(missing(zlim)) { zlim=NULL }
+    if(missing(bkgr)) { bkgr="white" }
+    
+
+  
+  opar = par(no.readonly = TRUE)
+  
+  NTOT = (J-I+1)
+  KROW =  floor(NTOT/5)
+  
+  if(KROW>=1)
+    {
+    par(mfrow=c(KROW,5),mai=c(0, 0, 0,0))
+  }
+  else
+    {
+      par(mfrow=c(1, NTOT),mai=c(0, 0, 0,0))
+
+    }
+
+  KMAX = min(c(J, 25))
+  for( i in I:KMAX)
+    {
+      
+      pltomo(MOD$x,MOD$y,MOD$MOD,i, col=colmap, zlim=zlim,  bkgr= bkgr, axes=FALSE, ann=FALSE )
+      
+      ##  image(xo, yo, TM1$MOD[[i]], col=tomo.colors(100), axes=FALSE, ann=FALSE) 
+
+      
+      text(MOD$x[1],MOD$y[1], labels=paste(sep=" : ", paste(sep="", "LAY=", i) , paste(sep="", "Z=",MOD$D[i])), adj=c(0,0) ,xpd=TRUE, font=2)
+      
+###   HOZscale(MOD$MOD[[i]] , col= rainbow(100)  , units="1/q", SIDE=2)
+###   image.SCALE(MOD$MOD[[i]] , col=rainbow(100), nlab=2)
+###  PROJmap(JAPmap,  ADD=TRUE, COL=TRUE)
+
+      box()
+        if(!is.null(MAP)) plotGEOmapXY(MAP, PROJ=MAP$PROJ, add=TRUE, xpd=FALSE)
+      
+      
+###     title(paste(sep=' ', "Depth", MOD$D[i], "-", MOD$D[i+1]))
+      
+    }
+
+  newpar= par(no.readonly = TRUE)
+  par(opar)
+
+  
+  invisible(newpar)
+}
+
